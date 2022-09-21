234b71ec81c4b0666b379683a2c5aadc9ad6418d
unknown <r.507.popi@outlook.jp>
2022-09-21 19:46:44 +0900

    20220921 commit

---
+++ ArgsLastPrinter.java
@@ -0,0 +1,9 @@
+public class ArgsLastPrinter{
+    public static void main(String[] args) {
+//args.lengthで引数の数をチェック。0個ならなしと出力。
+
+       if(args.length ==0){
+        System.out.println("なし");
+}
+    }
+}
