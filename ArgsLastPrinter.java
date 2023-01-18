359d028fb1989fd640d092c6251919819f354ced
unknown <r.507.popi@outlook.jp>
2023-01-18 13:41:50 +0900

    20230118

--- ArgsLastPrinter.java
+++ ArgsLastPrinter.java
@@ -1,9 +1,9 @@
 public class ArgsLastPrinter{
-    public static void main(String[] args) {
-//args.lengthで引数の数をチェック。0個ならなしと出力。
-
-       if(args.length ==0){
-        System.out.println("なし");
-}
+    public static void main(String[] args){
+        if(args.length == 0){
+            System.out.println("なし");
+        }else{
+            System.out.println(args[args.length - 1]);
+        }
     }
 }
--- public class HelloWorld {.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args)
-    {
-        person person = new Person();
-        person.speak();
-    }
-}
